a60d11e0c6c03147bd31073bc5b0b0a75f3638d6
Andres533-dev <andres.murillo2006@gmail.com>
2025-10-19 19:45:22 -0500

    Add ExperimentService class with component annotation

--- workshops/spring-ioc/Point 1 -The laboratory of beans/ACM-Taller1/src/main/java/ExperimentService.java
+++ workshops/spring-ioc/Point 1 -The laboratory of beans/ACM-Taller1/src/main/java/ExperimentService.java
@@ -1,4 +1,7 @@
 package Point1;
-
+@Component("ExperimentService")
 public class ExperimentService {
+    public ExperimentService() {
+         System.out.println("Se ha creado ExperimentService!");
+    }
 }
